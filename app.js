65e96c38b017a83b826cb3f77492d16a4b863db1
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-04-25 10:42:46 -0500

    started creating api routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('./app_server/models/db');
 
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+var routesApi = require('./app_api/index');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
